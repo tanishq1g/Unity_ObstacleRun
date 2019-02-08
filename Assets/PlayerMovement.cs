0f8e33024b2c4f58c4fbfdcdb0e725455f64bcec
tanishq1g <tanishq.gupta@iiitb.org>
2019-02-09 01:10:28 +0530

    programming

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //reference to Rigidbody component
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello, world");
+        //sets gravity
+        //rb.useGravity = false;
+        //sets forces in x, y, z direction respectively
+        rb.AddForce(0, 200, 500);
+    }
+
+
+    // marked FixedUpdate because we are for physics
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Add force of 2000 on y axis
+        rb.AddForce(0, 0, 200 * Time.deltaTime);
+    }
+}
