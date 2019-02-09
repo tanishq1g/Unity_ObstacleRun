d0afeb359e73815a0b4a99a1d63e3533e2d7141e
tanishq1g <tanishq.gupta@iiitb.org>
2019-02-09 09:45:31 +0530

    score and UI

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,19 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    //is called whenever player collides with something, player should have rigidbody and collider added to it 
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        //Debug.Log(collisionInfo.collider.name);
+
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit an Obstacle!");
+            movement.enabled = false;
+        }
+    }
+}
